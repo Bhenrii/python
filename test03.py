4c024cf6287fddfceaae31da958b534040d15ed0
Bruno Braz <brunohenriquebraz3@gmail.com>
2024-09-26 12:35:24 -0300

    Add files via upload

---
+++ test03.py
@@ -0,0 +1,4 @@
+day = input("What day is your birthday? ")
+month = input("What month is your birthday? ")
+year = input("What'is your birthday year? ")
+print("you were born on day", day, "of", month, "of", year,".Correto?") 
